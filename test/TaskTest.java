686dd1ae46cb853f6b09ade232f0eabbacac4321
East of Eden <azazal.combs@gmail.com>
2024-03-27 19:40:50 +0300

    Финальное задание 5го спринта. 1.Добавил тесты для методов удаления Task, Subtask, Epic 2.Изменил логику проверки Task, Subtask, Epic при изменении их id.

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -5,13 +5,10 @@ import tasks.Task;
 public class TaskTest {
     @Test
     public void shouldReturnPositiveWhenEqualTask() {
-        //Получаем объекты типа Task и присваиваем им одинаковые ID.
         Task taskActual = new Task("Найти работу", "Найти работу с зарплатой 1000к");
         taskActual.setIdTask(1);
         Task taskExpected = new Task("Найти работу", "Найти работу с зарплатой 1000к");
         taskExpected.setIdTask(1);
-
-        //Сраниваем таски.
         Assertions.assertEquals(taskExpected, taskActual, "Таски с одинаковыми id не равны");
     }
 
