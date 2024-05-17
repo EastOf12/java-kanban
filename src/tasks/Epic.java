fc3abc03bade0e63c7a94fc21baeafbf24eae909
East of Eden <azazal.combs@gmail.com>
2024-05-17 23:17:31 +0300

    Финальное задание 7 спринт

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Epic extends Task {
     private ArrayList<Integer> subtasks;
-    public Epic(String title, String description) {
 
+    public Epic(String title, String description) {
         super(title, description);
         subtasks = new ArrayList<>();
     }
