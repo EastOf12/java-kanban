dac073bc10d5f4729bacdc2b7783caf029b5c262
East of Eden <azazal.combs@gmail.com>
2024-05-17 22:57:49 +0300

    Финальное задание 7 спринт

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,8 +1,7 @@
 package tasks;
 
-public class Subtask extends Task {
+public class Subtask extends Task{
     private int idEpic;
-
     public Subtask(String title, String description, int idEpic) {
         super(title, description);
         this.idEpic = idEpic;
